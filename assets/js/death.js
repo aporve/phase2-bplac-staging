2f98bf97b6e4939f8c9c752eadbb34dfcc860911
aporve <aporvep@gmail.com>
2022-05-12 10:43:15 +0530

    update the code

--- assets/js/death.js
+++ assets/js/death.js
@@ -1149,7 +1149,7 @@ function handleFormAddBeneficiary(event) {
       (beneficiary["check2"] = dataBen.privacy_consent_beneficiary_2)
     BeneficiaryList.push(beneficiary)
 
-    $("#customer_Name").text(`Hi ${customName}. Hang in there as we process your request. Expect an SMS from us within 7 to 10 working on the status of your request.`)
+    $("#customer_Name").text(`Hi ${customName}. Hang in there as we process your request. Expect an SMS from us within 7 to 10 working days on the status of your request.`)
     if (buttonCount == 1) {
       data1 = dataBen
       console.log("data1", data1)
@@ -1733,7 +1733,7 @@ function handleForm(event) {
     $("#death_data_privacy").hide()
     $("#payment").show()
     customName = field_BeneficiaryFirstName
-    $("#customer_Name").text(`Hi ${field_BeneficiaryFirstName}. Hang in there as we process your request. Expect an SMS from us within 7 to 10 working on the status of your request.`)
+    $("#customer_Name").text(`Hi ${field_BeneficiaryFirstName}. Hang in there as we process your request. Expect an SMS from us within 7 to 10 working days on the status of your request.`)
     console.log("Data -> ", data)
   } else if (comapareDates == false && (field_DOB !== "" || field_DOID !== "")) {
     $("#popUp_DOB").modal("show")
