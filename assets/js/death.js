896831c598b0ee7afd660a08a24ce9ffab0e1945
aporve <aporvep@gmail.com>
2022-05-13 03:36:10 +0530

    Update the code

--- assets/js/death.js
+++ assets/js/death.js
@@ -1733,7 +1733,7 @@ function handleForm(event) {
     $("#death_data_privacy").hide()
     $("#payment").show()
     customName = field_BeneficiaryFirstName
-    $("#customer_Name").text(`Hi ${field_BeneficiaryFirstName}. Hang in there as we process your request. Expect an SMS from us within 7 to 10 working days on the status of your request.`)
+    $("#customer_Name").text(`Hang in there as we process your request. Expect an SMS from us within 7 to 10 working days on the status of your request.`)
     console.log("Data -> ", data)
   } else if (comapareDates == false && (field_DOB !== "" || field_DOID !== "")) {
     $("#popUp_DOB").modal("show")
