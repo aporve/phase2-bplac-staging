d9de0c14990ab4fc948e519bc13fa0b0565c2647
aporve <aporvep@gmail.com>
2022-06-06 00:19:33 +0530

    Update the code

--- assets/js/illness.js
+++ assets/js/illness.js
@@ -957,7 +957,7 @@ function handleForm(event) {
         $("#step2").addClass("active");
         $("#step2>div").addClass("active");
         $("#requirements").show();
-        $("#customer_Name").text(`Hi ${field_firstName}. Hang in there as we process your request. Expect an SMS from us within 7 to 10 working days on the status of your request.`);
+        $("#customer_Name").text(`Hi ${field_firstName}. Hang in there as we process your request. Expect an SMS from us within 1 to 2 working days on the status of your request.`);
         /* $('#requirements')[0].scrollIntoView(true); */
 
         console.log("Data -> ", data);
